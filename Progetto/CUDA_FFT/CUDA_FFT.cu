9c7e8fdbbfe41d252a515b8d8fd18b72a1dbef08
kevin <kevinkoltraka1011@gmail.com>
2024-12-08 12:37:36 +0100

    non funziona cudaDeviceSynchronize

--- Progetto/CUDA_FFT/CUDA_FFT.cu
+++ Progetto/CUDA_FFT/CUDA_FFT.cu
@@ -32,6 +32,21 @@ double cpuSecond() {
     return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
 }
 
+void checkResult(complex *hostRef, complex *gpuRef, const int N) {
+    double epsilon = 1.0E-8;
+    bool match = 1;
+
+    for (int i = 0; i < N; i++) {
+        if (abs(hostRef[i].real - gpuRef[i].real) > epsilon || abs(hostRef[i].imag - gpuRef[i].imag) > epsilon) {
+            match = 0;
+            printf("Arrays do not match!\n");
+            printf("host (%0.2f; %0.2f) gpu (%0.2f; %0.2f) at current %d\n", hostRef[i].real, hostRef[i].imag, gpuRef[i].real, gpuRef[i].imag, i);
+            break;
+        }
+    }
+    if (match)
+        printf("Arrays match.\n\n");
+}
 
 
 
@@ -93,6 +108,7 @@ void fft_inplace_recursive(complex *input, complex *output, int step, int N) {
         output[k].real = even.real + temp.real;
         output[k].imag = even.imag + temp.imag;
         // seconda metà delle frequenza ottenuta per simmetria
+        // (temp con segno meno dato che il twiddle della seconda metà ha segno opposto)
         output[k + N/2].real = even.real - temp.real;
         output[k + N/2].imag = even.imag - temp.imag;
     }
@@ -168,11 +184,108 @@ void convert_to_short(complex *input, short *output, int N) {
     }
 }
 
-void do_fft_stuff_on_host(const char* filename) {
+
+
+
+/* KERNEL GPU */
+
+
+
+ 
+__global__ void fft_device_side(complex *input, complex *output, int step, int N) {
+    /*
+        Quando lancio un'altra griglia questo si ripete... è giusto???
+        Penso di si, è un sostituto di k
+    */
+    int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // Grazie alla simmetria della DFT, per calcolare N campioni della trasformata 
+    // ho bisogno di soli N/2 thread
+    int num_threads = N/2;  
+
+    if (N == 1) {
+        output[0] = input[0];
+
+        return;
+    }
+
+    /*
+        Solamente il primo blocco di ogni stadio lancia un kernel ricorsivo?
+        Sono abbastanza sicuro di si!
+    */
+    if(global_idx == 0) {
+        /*
+            Ogni campione k della trasformata necessita
+            di TUTTI i campioni dello stadio precedente, divisi
+            in campioni pari e dispari. 
+        */
+
+        /*
+            Lancio due kernel da N/2 thread
+        */
+        int threads_per_block = 1024;
+        int num_blocks = (num_threads + threads_per_block-1) / threads_per_block;
+
+        fft_device_side<<<num_blocks, threads_per_block>>>(input, output, step*2, N/2);
+        fft_device_side<<<num_blocks, threads_per_block>>>(input + step, output + N/2, step*2, N/2);
+
+        /*
+            Qua c'è da sincronizzare per forza?
+            La componente k-esima della trasformata ha delle dipendenze 
+            con quelle calcolate dagli altri thread? 
+            Devo sincronizzare anche prima?
+
+            Occhio che a quanto pare è deprecata
+        */
+        //cudaDeviceSynchronize();
+    }
+    __syncthreads();
+
+
+    // Combina i risultati
+    
+    // Calcolo del twiddle factor
+    // NB: Qua sto ripetendo il calcolo del twiddle molte volte
+    // forse posso risparmiarmi queste ripetizioni con la memoria confivisa?
+    double phi = (-2*PI/N) * global_idx;  // Segno negativo per la FFT     
+    complex twiddle = {
+        cos(phi),
+        sin(phi)
+    };
+
+    complex even = output[global_idx];
+
+    complex temp = {
+        twiddle.real * output[global_idx + N/2].real - twiddle.imag * output[global_idx + N/2].imag,
+        twiddle.real * output[global_idx + N/2].imag + twiddle.imag * output[global_idx + N/2].real
+    };
+
+    output[global_idx].real = even.real + temp.real;
+    output[global_idx].imag = even.imag + temp.imag;
+    //relazione simmetrica
+    output[global_idx + N/2].real = even.real - temp.real;
+    output[global_idx + N/2].imag = even.imag - temp.imag;
+}
+
+
+
+__global__ void sumArrayOnGPU(float *A, float *B, float *C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N) C[i] = A[i] + B[i];
+}
+
+
+
+
+int main(int argc, char **argv) {
+    // Configuration parameters
+    const char* FILE_NAME = "test_voce.wav";
+    
+    /* ESECUZIONE SOLO LATO HOST */
+
     drwav wav_in;
-    if (!drwav_init_file(&wav_in, filename, NULL)) {
-        fprintf(stderr, "Errore nell'aprire il file %s.wav.\n", filename);
-        exit(1);
+    if (!drwav_init_file(&wav_in, FILE_NAME, NULL)) {
+        fprintf(stderr, "Errore nell'aprire il file %s.wav.\n", FILE_NAME);
+        return 1;
     }
 
     size_t num_samples = wav_in.totalPCMFrameCount * wav_in.channels;
@@ -186,44 +299,42 @@ void do_fft_stuff_on_host(const char* filename) {
         num_samples = padded_samples;
     }
 
-    // Alloco memoria per:
-    //  - dati audio (PCM a 16 bit)
-    //  - dati audio (PCM a 16 bit) convertiti in numeri complessi
-    //  - dati delle sinusoidi prodotte dalla fft
+    /*
+        Alloco memoria per:
+            - campioni PCM a 16 bit del file di ingresso
+            - campioni PCM a 16 bit del file di ingresso convertiti in numeri complessi
+            - campioni della trasformata ottenuti con FFT
+    */
     short* signal_samples = (short*)malloc(num_samples * sizeof(short));
     if (signal_samples == NULL) {
         fprintf(stderr, "Errore nell'allocazione della memoria.\n");
-        exit(1);
+        return 1;
     }
     complex* complex_signal_samples = (complex*)malloc(num_samples * sizeof(complex));
     if (complex_signal_samples == NULL) {
         fprintf(stderr, "Errore nell'allocazione della memoria.\n");
-        exit(1);
+        return 1;
     }
     complex* fft_samples = (complex*)malloc(num_samples * sizeof(complex));
     if (fft_samples == NULL) {
         fprintf(stderr, "Errore nell'allocazione della memoria.\n");
-        exit(1);
+        return 1;
     }
 
     // Lettura dei dati audio dal file di input
     size_t frames_read = drwav_read_pcm_frames_s16(&wav_in, wav_in.totalPCMFrameCount, signal_samples);
     if (frames_read != wav_in.totalPCMFrameCount) {
         fprintf(stderr, "Errore durante la lettura dei dati audio.\n");
-        exit(1);
+        return 1;
     }
     drwav_uninit(&wav_in); 
-
-
-
 
     // calcolo la FFT sull'host
     convert_to_complex(signal_samples, complex_signal_samples, num_samples);
     double start = cpuSecond();
     fft_host_side(complex_signal_samples, fft_samples, num_samples);
-    double elapsed = cpuSecond() - start;
-    // printf("sumArrayOnHost Time elapsed %f sec\n", elapsed);
-
+    double elapsed_host = cpuSecond() - start;
+    // printf("sumArrayOnHost Time elapsed %f sec\n", elapsed_host);
 
 
 
@@ -231,7 +342,7 @@ void do_fft_stuff_on_host(const char* filename) {
     FILE *output_file = fopen("amplitude_spectrum.txt", "w");
     if (output_file == NULL) {
         fprintf(stderr, "Errore nell'aprire il file di output.\n");
-        exit(1);
+        return 1;
     }
 
     for (int i = 0; i < num_samples; i++) {
@@ -250,211 +361,6 @@ void do_fft_stuff_on_host(const char* filename) {
 
 
 
-    /* --- PARTE IFFT --- */
-
-
-
-    // inizializzazione dati
-    char generated_filename[100];   //dimensione arbitraria perchè non ho voglia
-    sprintf(generated_filename, "IFFT-generated-%s", filename);
-    // mi assicuro di non imbrogliare riciclando i dati di prima
-    memset(signal_samples, 0, num_samples*sizeof(short));
-    memset(complex_signal_samples, 0, num_samples);
-
-    // Preparazione del formato del file di output
-    drwav_data_format format_out;
-    format_out.container = drwav_container_riff;
-    format_out.format = DR_WAVE_FORMAT_PCM;
-    format_out.channels = 1;              // Mono
-    format_out.sampleRate = SAMPLE_RATE;  // Frequenza di campionamento
-    format_out.bitsPerSample = 16;        // 16 bit per campione
-
-    // Inizializzazione del file di output
-    drwav wav_out;
-    if (!drwav_init_file_write(&wav_out, generated_filename, &format_out, NULL)) {
-        fprintf(stderr, "Errore nell'aprire il file di output %s.\n", generated_filename);
-        exit(1);
-    }
-    
-    ifft_host_side(fft_samples, complex_signal_samples, num_samples);
-    convert_to_short(complex_signal_samples, signal_samples, num_samples);
-
-    // Scrittura dei dati audio nel file di output
-    drwav_write_pcm_frames(&wav_out, num_samples, signal_samples);
-    drwav_uninit(&wav_out); // Chiusura del file di output
-
-    printf("File WAV %s creato con successo\n", generated_filename);
-
-    free(signal_samples);
-    free(complex_signal_samples);
-    free(fft_samples);
-}
-
-
-
-
-
-
-
-
-/* KERNEL GPU */
-
-
-
-
-
-// __global__ void fft_device_side(complex *x, complex *X, int N) {
-//     /*
-//         Quando lancio un'altra griglia questo si ripete... è giusto???
-//     */
-//     int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-//     // Controllo se N è una potenza di 2
-//     if((N & (N - 1)) != 0) {
-//         fprintf(stderr, "Errore: N (%d) non è una potenza di 2.\n", N);
-//         exit(1);
-//     }
-
-//     if(N == 1) {
-//         // Caso base: copia l'unico elemento
-//         X[0].real = x[0].real;
-//         X[0].imag = x[0].imag;
-
-//         return;
-//     }
-
-//     /*
-//         OCCHIO QUESTO è SBAGLIATO!
-//         La memoria locale è privata per ogni thread
-
-//         Mi sa che mi tocca allocare dinamicamente con CudaMalloc...
-//         (e anche deallocare con CudaFree)
-
-//         Posso usare device?
-//     */
-    
-//     complex signal_even[N/2];
-//     complex signal_odd[N/2];
-//     complex trasformata_even[N/2];
-//     complex trasformata_odd[N/2];
-
-//     cudaMalloc((void**)&signal_even, N/2*sizeof(complex));
-//     cudaMalloc((void**)&signal_odd, N/2*sizeof(complex));
-//     cudaMalloc((void**)&trasformata_even, N/2*sizeof(complex));
-//     cudaMalloc((void**)&trasformata_odd, N/2*sizeof(complex));
-
-//     /*
-//         Solamente il primo blocco di ogni stadio lancia un kernel ricorsivo?
-//         Sono abbastanza sicuro di si!
-//     */
-//     if(global_idx == 0) {
-//         /*
-//         Ogni frequenza k della trasformata necessita
-//         di TUTTI i campioni dello stadio precedente, divisi
-//         in campioni pari e dispari. Come faccio a selezionarli 
-//         con ogni thread.
-
-//         Forse questo lo posso preparare lato CPU 
-//         passando alla funzione un array di array
-//         indicizzato con il tid???
-//         */
-
-//         // Separazione dei campioni pari e dispari
-//         for(int i = 0; i < N/2; i++) {
-//             signal_even[i] = x[2*i];
-//             signal_odd[i] = x[2*i + 1];
-//         }
-
-//         int threads_per_block = 1024;
-//         int num_blocks = (N + threads_per_block-1) / threads_per_block;
-
-//         // Ricorsivamente calcola la FFT per pari e dispari
-//         fft_device_side<<<num_blocks, threads_per_block>>>(signal_even, trasformata_even, N/2);
-//         fft_device_side<<<num_blocks, threads_per_block>>>(signal_odd, trasformata_odd, N/2);
-
-//         /*
-//             Qua c'è da sincronizzare per forza dato?
-//             La componente k-esima della trasformata ha delle dipendenze 
-//             con quelle calcolate dagli altri thread? 
-
-//             cuda_device_synchronize();
-//         */
-//     }
-
-
-//     /*
-//         Qua al posto di un ciclo avrà N/2 thred che mi calcolano
-//         le trasformate dei vari stadi 
-
-//         k === tid; o un qualcosa del genere
-//     */
-
-//     // Combina i risultati
-//     for(int k = 0; k < N/2; k++) {
-//         double phi = (-2*PI/N) * k;
-//         // Calcolo del twiddle factor
-//         complex twiddle = {
-//             cos(phi),
-//             sin(phi)
-//         };
-
-//         // temp === prodotto tra twiddle e la trasformata dei dispari (rende più leggibile sotto)
-//         complex temp = {
-//             twiddle.real * trasformata_odd[k].real - twiddle.imag * trasformata_odd[k].imag,
-//             twiddle.real * trasformata_odd[k].imag + twiddle.imag * trasformata_odd[k].real
-//         };
-
-//         // Combina i risultati nella trasformata finale
-//         X[k].real = trasformata_even[k].real + temp.real;
-//         X[k].imag = trasformata_even[k].imag + temp.imag;
-//         // La seconda metà è calcolata grazie alle relazioni simmetriche dei termini esponenziali
-//         // (temp con segno meno dato che il twiddle della seconda metà ha segno opposto)
-//         X[k + N/2].real = trasformata_even[k].real - temp.real;
-//         X[k + N/2].imag = trasformata_even[k].imag - temp.imag;
-//     }
-
-//     free(signal_even);
-//     free(signal_odd);
-//     free(trasformata_even);
-//     free(trasformata_odd);
-// }
-
-
-
-__global__ void sumArrayOnGPU(float *A, float *B, float *C, int N) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N) C[i] = A[i] + B[i];
-}
-
-
-
-
-int main(int argc, char **argv) {
-    // Configuration parameters
-    const char* FILE_NAME = "test_voce.wav";
-    
-    printf("%s Starting...\n", argv[0]);
-
-
-    do_fft_stuff_on_host(FILE_NAME);
-
-
-
-
-
-
-    /* ESECUZIONE SOLO LATO HOST */
-    /* TODO: refactor in una funzione a parte */
-
-
-
-
-
-
-
-
-
-
     
 
 
@@ -464,14 +370,17 @@ int main(int argc, char **argv) {
 
 
     /* ESECUZIONE CON GPU */
-    /* TODO: refactor in una funzione a parte */
 
 
 
 
 
 
-    printf("Vuoi rifare con la GPU?\n");
+
+
+
+
+    printf("\nSchiaccia qualsiasi tasto per rifare con la GPU...\n");
     getchar();
 
     // Set up device
@@ -481,60 +390,94 @@ int main(int argc, char **argv) {
     printf("Using Device %d: %s\n", dev, deviceProp.name);
     CHECK(cudaSetDevice(dev));
 
-    
     /*
-        Numero di frequenze da calcolare: N/2
+        Grazie alla simmetria della DFT, per calcolare N campioni
+        della trasformata ho bisogno di soli N/2 thread
+
+        Fai un po' di trial and error con threads_per_block
     */
-    int numThreads = 1 << 20;  
-    /*
-        Fai un po' di trial and error, direi di partire con 1024
-    */
-    int threadsPerBlock = 1024;
-    int numBlocks = (numThreads + threadsPerBlock - 1) / threadsPerBlock;
+    int num_threads = num_samples/2;  
+    int threads_per_block = 1024;
+    int num_blocks = (num_threads + threads_per_block - 1) / threads_per_block;
 
-    // // Alloca memoria per i campioni sul device
-    // short* device_signal_samples;
-    // complex* device_dft_samples
+    // Alloca memoria per i campioni sul device
+    complex* device_complex_signal_samples;
+    complex* device_fft_samples;
     
-    // CHECK(cudaMalloc((short**)&device_signal_samples, num_samples*sizeof(short)));
-    // CHECK(cudaMalloc((complex**)&device_dft_samples, (num_samples/2)*sizeof(complex)));
+    CHECK(cudaMalloc(&device_complex_signal_samples, num_samples*sizeof(complex)));
+    CHECK(cudaMalloc(&device_fft_samples, num_samples*sizeof(complex)));
     
-    // CHECK(cudaMemcpy(device_signal_samples, signal_samples, num_samples*sizeof(short), cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(device_complex_signal_samples, complex_signal_samples, num_samples*sizeof(complex), cudaMemcpyHostToDevice));
+    CHECK(cudaMemset(device_fft_samples, 0, num_samples*sizeof(complex)));
+
+
+    // Invoke kernel
+    start = cpuSecond();
+    fft_device_side<<<num_blocks, threads_per_block>>>(device_complex_signal_samples, device_fft_samples, num_samples, 1);
+    CHECK(cudaDeviceSynchronize());
+    double elapsed_device  = cpuSecond() - start;
+
+    // Copy kernel result back to host side
+    complex* gpu_ref_fft_samples = (complex *)malloc(num_samples);
+    CHECK(cudaMemcpy(gpu_ref_fft_samples, device_fft_samples, num_samples, cudaMemcpyDeviceToHost));
+    
+    checkResult(fft_samples, gpu_ref_fft_samples, num_samples);
+    printf("Host: %f\n", elapsed_host);
+    printf("Device: %f\n", elapsed_device);
+    printf("SPEEDUP: %f\n", elapsed_host/elapsed_device);
+    getchar();
+
+
+    /* --- PARTE IFFT --- */
+
+
+
+    
+
+    // Preparazione del formato del file di output
+    drwav_data_format format_out;
+    format_out.container = drwav_container_riff;
+    format_out.format = DR_WAVE_FORMAT_PCM;
+    format_out.channels = 1;              // Mono
+    format_out.sampleRate = SAMPLE_RATE;  // Frequenza di campionamento
+    format_out.bitsPerSample = 16;        // 16 bit per campione
+
+    // inizializzazione dati
+    char generated_filename[100];  
+    sprintf(generated_filename, "GPU-IFFT-generated-%s", FILE_NAME);
+
+    // Inizializzazione del file di output
+    drwav wav_out;
+    if (!drwav_init_file_write(&wav_out, generated_filename, &format_out, NULL)) {
+        fprintf(stderr, "Errore nell'aprire il file di output %s.\n", generated_filename);
+        return 1;
+    }
+
+    // mi assicuro di non imbrogliare riciclando i dati di prima
+    memset(signal_samples, 0, num_samples*sizeof(short));
+    memset(complex_signal_samples, 0, num_samples);
+    
+    ifft_host_side(gpu_ref_fft_samples, complex_signal_samples, num_samples);
+    convert_to_short(complex_signal_samples, signal_samples, num_samples);
+
+    // Scrittura dei dati audio nel file di output
+    drwav_write_pcm_frames(&wav_out, num_samples, signal_samples);
+    drwav_uninit(&wav_out); // Chiusura del file di output
+
+    printf("File WAV %s creato con successo\n", generated_filename);
 
 
 
 
 
-    // // TODO: continualo...
+    // Free device global memory
+    CHECK(cudaFree(device_complex_signal_samples));
+    CHECK(cudaFree(device_fft_samples));
+    // Free host memory
+    free(signal_samples);
+    free(complex_signal_samples);
+    free(fft_samples);
+    free(gpu_ref_fft_samples);
 
-
-
-
-    // // Invoke kernel
-    // int gridSize = (nElem + blockSize - 1) / blockSize;
-
-    // iStart = cpuSecond();
-    // sumArrayOnGPU<<<gridSize, blockSize>>>(d_A, d_B, d_C, nElem);
-    // CHECK(cudaDeviceSynchronize());
-    // iElaps = cpuSecond() - iStart;
-    // printf("sumArrayOnGPU <<<%d, %d>>> Time elapsed %f sec\n", gridSize, blockSize, iElaps);
-
-    // // Copy kernel result back to host side
-    // CHECK(cudaMemcpy(gpuRef, d_C, nBytes, cudaMemcpyDeviceToHost));
-
-    // // Check device results
-    // checkResult(hostRef, gpuRef, nElem);
-
-    // // Free device global memory
-    // CHECK(cudaFree(d_A));
-    // CHECK(cudaFree(d_B));
-    // CHECK(cudaFree(d_C));
-
-    // // Free host memory
-    // free(h_A);
-    // free(h_B);
-    // free(hostRef);
-    // free(gpuRef);
-
-    // return 0;
+    return 0;
 }
